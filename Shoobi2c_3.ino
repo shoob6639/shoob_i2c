4c46588049811d8bc844996886094a876e7af8a6
Shoob <42870953+Soggy-Bread@users.noreply.github.com>
2025-11-20 22:54:14 -0500

    V2
    Got my hands on a logic analyzer. Confirmed send commands are working at low clock speeds (delay set to 100ms between most commands)
    
    Once all commands are working, timing will be converted to CPU clocks most likely

--- Shoobi2c_3.ino
+++ Shoobi2c_3.ino
@@ -1,23 +1,23 @@
 #define SDA_HI PORTB = (PORTB & ~(1 << 3)) | 0x08 
-#define SDA_LO PORTB = (PORTB & ~(1 << 3)) | 0x00
+#define SDA_LO PORTB = (PORTB & ~(1 << 3)) | 0x00 
 
 #define SCL_HI PORTB = (PORTB & ~(1 << 4)) | 0x16 
-#define SCL_LO PORTB = (PORTB & ~(1 << 4)) | 0x00
+#define SCL_LO PORTB = (PORTB & ~(1 << 4)) | 0x00 
 
 void setup() {
+
   DDRB = 0xFF;
-  PORTB = 0x00;
+
   SDA_HI;
   SCL_HI;
 }
 
-
+int X=100; //TIME INTERVAL
 
 void I2C_START(int X){
   //I2C START 
   //Pull SCL / SDA HI
   SDA_HI;
-  delay(X);
   SCL_HI;
   delay(X);
   //Pull SDA LO, WAIT, PULL SCL LO
@@ -25,68 +25,54 @@ void I2C_START(int X){
   delay(X);
   SCL_LO;
   delay(X);
+
 }
 
 void I2C_STOP(int X){
-  //I2C START 
-  //Pull SCL / SDA HI
   SDA_LO;
   delay(X);
   SCL_LO;
   delay(X);
-
-  //Pull SCL HI
-  //WAIT
-  //PULL SDA HI
-
+  //Pull SCL / SDA HI
   SCL_HI;
   delay(X);
   SDA_HI;
   delay(X);
 }
 
-void I2C_SEND(byte SDA, int X){
+void I2C_SEND(byte SDA){
   //Transmit Byte
-  for(int i = 8; i>= 0; i--) {
-    (SDA & 0x80) ? SDA_LO : SDA_HI; 
-    SDA <<= 1;
-    delay(X);
-    SCL_LO;
-    delay(X);
-    SCL_HI;   
-    delay(X);
-  }
-
+  delay(1);
   SDA_HI;
+  for(int i = 7; i>= 0; i--) {
+    delay(1);
+    SCL_HI;
+    (SDA & 0x80) ? SDA_HI : SDA_LO; 
+    SDA <<= 1;
+    delay(99);
+    SCL_LO;
+  }
+  delay(1);
+  SDA_LO;
+  delay(1);
   SCL_HI;
+  delay(1);
+  SCL_LO;
+  delay(1);
 }
+
+
 
 void loop(){
 
-SDA_HI;
-SCL_HI;
-
-delay(2000);
-
-I2C_START(20);
-I2C_SEND(0x48, 20); //H
-I2C_SEND(0x45, 20); //E
-I2C_SEND(0x4c, 20); //L
-I2C_SEND(0x4c, 20); //L
-I2C_SEND(0x4f, 20); //O
-I2C_STOP(20);
-
-delay(1000);
-
-I2C_START(20);
-I2C_SEND(0x57, 20); //W
-I2C_SEND(0x4f, 20); //O
-I2C_SEND(0x52, 20); //R
-I2C_SEND(0x4c, 20); //L
-I2C_SEND(0x44, 20); //D
-I2C_SEND(0x21, 20); //!
-I2C_STOP(20);
-
-delay(5000);
-
+I2C_START(X);
+I2C_SEND(0XAA);
+I2C_SEND(0XFF);
+I2C_SEND(0X13);
+I2C_SEND(0X38);
+I2C_SEND(0X4B);
+I2C_STOP(X);
+while(1) {
+    //do nothing in here
+  }
 }
